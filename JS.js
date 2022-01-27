ddc429ab884a6db1367e8b599443affa18206d16
KAMIL ANONIMUS <98107894+KamilAnonimus@users.noreply.github.com>
2022-01-27 11:30:12 +0300

    Site
    Site

---
+++ JS.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    new SimpleAdaptiveSlider('.banner', {
+        loop: true,
+        autoplay: false,
+        interval: 5000,
+        swipe: true,
+      });
+    });

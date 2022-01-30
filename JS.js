c0af8f4b039f96b91e3cd771f9edb1ef37a2ab96
KAMIL ANONIMUS <98107894+KamilAnonimus@users.noreply.github.com>
2022-01-30 11:20:20 +0300

    site
    site

--- JS.js
+++ JS.js
@@ -1,5 +1,27 @@
-$(document).ready(function() {
-	let position = 0;
-	const slidesToScroll = 1;
-	const slidesToScroll = 1;
-}
+// $(document).ready(function() {
+// 	let position = 0;
+// 	const slidesToShow = 1;
+// 	const slidesToScroll = 1;
+// 	const container = $('.slider-container');
+// 	const track = $('.slider-track');
+// 	const item = $('.slider-item');
+// 	var btnPrev = getElementId('.btn-prev');
+// 	const btnNext = $('.btn-next');
+// 	const itemWidht = container.widht() / slidesToShow;
+
+// 	item.each(function (index, item) {
+// 		$(item).css({
+// 			minWidht: itemWidht,
+// 		});
+
+// 	});
+ 
+// 	btnPrev.click(function(){
+// 		console.log('btnPrev');
+// 	});
+
+// 	btnNext.click(function(){
+// 		console.log('btnNext');
+// 	});
+// });
+

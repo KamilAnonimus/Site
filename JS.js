ef981014128f9e59ed1dbc1d1791edfcab700430
KAMIL ANONIMUS <98107894+KamilAnonimus@users.noreply.github.com>
2022-01-31 11:46:35 +0300

    Site
    Site

--- JS.js
+++ JS.js
@@ -1,27 +1,33 @@
-// $(document).ready(function() {
-// 	let position = 0;
-// 	const slidesToShow = 1;
-// 	const slidesToScroll = 1;
-// 	const container = $('.slider-container');
-// 	const track = $('.slider-track');
-// 	const item = $('.slider-item');
-// 	var btnPrev = getElementId('.btn-prev');
-// 	const btnNext = $('.btn-next');
-// 	const itemWidht = container.widht() / slidesToShow;
 
-// 	item.each(function (index, item) {
-// 		$(item).css({
-// 			minWidht: itemWidht,
-// 		});
+    let position = 0;
+    const slidesToShow = 2;
+    const slidesToScroll = 1;
+    const conatainer = $('.slider-container1');
+    const track = $('.slider-track');
+    const item = $('.slider-item');
+    const movementRight = $('.btn-prev');
+    const movementLeft = $('.btn-next');
+    const itemWidht = container1.widht() / slidesToShow;
+    const movePosition = slidesToScroll * itemWidht;
 
-// 	});
- 
-// 	btnPrev.click(function(){
-// 		console.log('btnPrev');
-// 	});
+    item.each(function (index, item) {
+        $(item).css({
+            minWidht:itemWidht,
+        });
+    });
 
-// 	btnNext.click(function(){
-// 		console.log('btnNext');
-// 	});
-// });
+function movementRight () {
+    position += movePosition;
+    
+    track.css({
+        transform:`translateX(${movePosition}px)`
+    });
+};
 
+function movementLeft () {
+    position -= movePosition;
+    
+    track.css({
+        transform:`translateX(${movePosition}px)`
+    });
+};

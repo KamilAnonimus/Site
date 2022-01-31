ef9cb2a5a5fb6714498e44c5d6971d9240dddb7b
KAMIL ANONIMUS <98107894+KamilAnonimus@users.noreply.github.com>
2022-01-31 21:33:59 +0300

    Site
    Site

--- JS.js
+++ JS.js
@@ -1,33 +1,33 @@
+    // let position = 0;
+    // const slidesToShow = 1;
+    // const slidesToScroll = 1;
+    // const container1 = document.getElementsByClassName('slider-container1');
+    // var track = document.getElementsByClassName('slider-track');
+    // function item () {document.getElementsByClassName('.slider-item');};
+    // var movementRight = document.getElementsByClassName('.btn-prev');
+    // function movementLeft () {document.getElementsByClassName('.btn-next');};
+    // function itemWidht () {container1.widht() / slidesToShow;};
+    // const movePosition = slidesToScroll * itemWidht;
 
-    let position = 0;
-    const slidesToShow = 2;
-    const slidesToScroll = 1;
-    const conatainer = $('.slider-container1');
-    const track = $('.slider-track');
-    const item = $('.slider-item');
-    const movementRight = $('.btn-prev');
-    const movementLeft = $('.btn-next');
-    const itemWidht = container1.widht() / slidesToShow;
-    const movePosition = slidesToScroll * itemWidht;
+    // (function each(index, item) {
+    //     $(item).css({
+    //         minWidht: itemWidht,
+    //     });
+    // });
 
-    item.each(function (index, item) {
-        $(item).css({
-            minWidht:itemWidht,
-        });
-    });
-
-function movementRight () {
-    position += movePosition;
+//     document.querySelector('#sumbit').onclick = function () {
+//         $(track).css(
+//             left:100px;
+//         );
+//     };
+// //     (function movementLeft () {
+//         position -= movePosition;
     
-    track.css({
-        transform:`translateX(${movePosition}px)`
-    });
-};
+//         $(track).css({
+//         transform:`translateX(${movePosition}px)`
+//     });
+// });
 
-function movementLeft () {
-    position -= movePosition;
-    
-    track.css({
-        transform:`translateX(${movePosition}px)`
-    });
-};
+document.querySelector('#image').addEventListener('click', () => {
+    document.querySelector('btn-prev').classList.add('slider-item1');
+  }); 

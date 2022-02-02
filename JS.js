615ea4601ffe4a413f6359ee0f86cffa393f3cdd
KAMIL ANONIMUS <98107894+KamilAnonimus@users.noreply.github.com>
2022-02-02 19:21:03 +0300

    Slider
    Slider

--- JS.js
+++ JS.js
@@ -1,33 +1,10 @@
-    // let position = 0;
-    // const slidesToShow = 1;
-    // const slidesToScroll = 1;
-    // const container1 = document.getElementsByClassName('slider-container1');
-    // var track = document.getElementsByClassName('slider-track');
-    // function item () {document.getElementsByClassName('.slider-item');};
-    // var movementRight = document.getElementsByClassName('.btn-prev');
-    // function movementLeft () {document.getElementsByClassName('.btn-next');};
-    // function itemWidht () {container1.widht() / slidesToShow;};
-    // const movePosition = slidesToScroll * itemWidht;
+const track = document.querySelector('.slider-track'),
+      buttonsLeft = document.querySelector('.btn-prev'),
+      buttonsRight = document.querySelector('.btn-next');
 
-    // (function each(index, item) {
-    //     $(item).css({
-    //         minWidht: itemWidht,
-    //     });
-    // });
+var position = 0
 
-//     document.querySelector('#sumbit').onclick = function () {
-//         $(track).css(
-//             left:100px;
-//         );
-//     };
-// //     (function movementLeft () {
-//         position -= movePosition;
-    
-//         $(track).css({
-//         transform:`translateX(${movePosition}px)`
-//     });
-// });
-
-document.querySelector('#image').addEventListener('click', () => {
-    document.querySelector('btn-prev').classList.add('slider-item1');
-  }); 
+function change_css(){
+    position += 1200
+    document.getElementById('slider-track').style.cssText = `right:${position}px;`;
+}

e13c4923b9f37f9742f0d3690369cdd8e6a2dc60
KAMIL ANONIMUS <98107894+KamilAnonimus@users.noreply.github.com>
2022-02-09 14:58:25 +0300

    slider
    slider

--- JS.js
+++ JS.js
@@ -1,15 +1,14 @@
-const track = document.querySelector('.slider-track'),
-      buttonsLeft = document.querySelector('.btn-prev'),
-      buttonsRight = document.querySelector('.btn-next');
-
-var position = 0
-
-function prev(){
-    position += 1200
-    document.getElementById('slider-track').style.cssText = `right:${position}px;`;
-}
-
-function next(){
-    position -= 1200
-    document.getElementById('slider-track').style.cssText = `right:${position}px;`;
-}
+(function() {
+    document.querySelector('.carousel-module').addEventListener('click', function(e) {
+      var first, last, parent;
+      parent = document.querySelector('.img-container');
+      first = parent.querySelector('.item-img');
+      last = parent.querySelector('.item-img:last-child');
+      if (e.target.classList.contains('arrow-left')) {
+        parent.appendChild(first);
+      }
+      if (e.target.classList.contains('arrow-right')) {
+        parent.insertBefore(last, first);
+      }
+    });
+  }).call(this);

b1accd8770875cd0debd5c8f257c1fb3e9a2fddf
KAMIL ANONIMUS <98107894+KamilAnonimus@users.noreply.github.com>
2022-02-28 13:01:20 +0300

    Site
    Site

--- JS.js
+++ JS.js
@@ -1,13 +1,25 @@
 (function(a) {
-  let timerId = setTimeout(function tick() {
-  var parent = document.querySelector('.img-container');
-  var first = parent.querySelector('.item-img');
-  parent.appendChild(first);
-  timerId = setTimeout(tick, 7000);
-  }, 7000);
-  document.querySelector('.img-container').addEventListener('mouseover', function (b) {
-    clearTimeout(timerId);
-  });
+  // let timerId = setTimeout(function tick() {
+  // var parent = document.querySelector('.img-container');
+  // var first = parent.querySelector('.item-img');
+  // parent.appendChild(first);
+  // timerId = setTimeout(tick, 7000);
+  // }, 7000);
+  // document.querySelector('.img-container').addEventListener('mouseover', function (b) {
+  //   clearTimeout(timerId);
+  // });
+
+  // document.querySelector('.img-container').addEventListener('mouseout', function (b) {
+  //     let timerId = setTimeout(function tick() {
+  //     var parent = document.querySelector('.img-container');
+  //     var first = parent.querySelector('.item-img');
+  //     parent.appendChild(first);
+  //     timerId = setTimeout(tick, 7000);
+  //     }, 7000);
+  //     document.querySelector('.img-container').addEventListener('mouseover', function (b) {
+  //       clearTimeout(timerId);
+  //     });
+  // });
 
   document.querySelector('.carousel-module').addEventListener('click', function(e) {
     var first, last, parent;

0821e8bbf4fee4523b765b2416053c7684118c33
KAMIL ANONIMUS <98107894+KamilAnonimus@users.noreply.github.com>
2022-03-04 21:30:44 +0300

    Slider
    Slider

--- JS.js
+++ JS.js
@@ -1,3 +1,33 @@
+document.querySelector('.carousel-module').addEventListener('touchstart', handleTouchStart, false);
+document.querySelector('.carousel-module').addEventListener('touchmove', handleTouchMove, false);
+let x1 = null;
+let y1 = null;
+function handleTouchStart(event) {
+  const firstTouch = event.touches[0];
+  x1 = firstTouch.clientX;
+  y1 = firstTouch.clientY
+}
+
+function handleTouchMove(event) {
+  var parent = document.querySelector('.img-container');
+  var first = parent.querySelector('.item-img');
+  var last = parent.querySelector('.item-img:last-child');
+  if (!x1 || !y1) {
+    return false;
+  }
+  let x2 = event.touches[0].clientX;
+  let y2 = event.touches[0].clientY;
+  let xDiff = x2 - x1;
+  let yDiff = y2 - y1;
+
+  if (Math.abs(xDiff) > Math.abs(yDiff)) {
+    if (xDiff > 0) parent.insertBefore(last, first);
+    else parent.appendChild(first);
+  }
+  x1 = null;
+  y1 = null;
+}
+
 (function(a) {
   let timerId = setTimeout(function tick() {
   var parent = document.querySelector('.img-container');

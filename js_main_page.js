3e47f295e7a0d7604f4715e16bee424d14ffa912
KamilAnonimus <anonimuskamil@yandex.ru>
2022-04-26 19:25:29 +0300

    site

--- js_main_page.js
+++ js_main_page.js
@@ -17,3 +17,22 @@ document.querySelector('#elastic').oninput = function(){
         });
     }
 }
+
+var categori = document.querySelector('#categories');
+var arrow = document.querySelector('#arrow');
+var ticks1 = document.querySelector('#ticks1');
+var i = 0
+categori.onclick = function() {
+    i += 1
+    function even(i){
+        if(Number.isInteger(i) == true){return i % 2 == 0}else{return undefined}
+     }
+    if (even(i) === true) {
+        arrow.style.transform = 'rotate(-45deg)';
+        ticks1.style.display = 'block';
+    }
+    if (even(i) === false) {
+        arrow.style.transform = 'rotate(-225deg)';
+        ticks1.style.display = 'none';
+    }
+}
